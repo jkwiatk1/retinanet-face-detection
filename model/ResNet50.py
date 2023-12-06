6a2e4ddfead983927b176665f1a3c44ec82c5924
jkwiatk1 <01142696@pw.edu.pl>
2023-12-06 12:05:41 +0100

    Init RetinaNet

--- model/ResNet50.py
+++ model/ResNet50.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
+
 class ResNet50(nn.Module):
     def __init__(self):
         super(ResNet50, self).__init__()
---
+++ model/RetinaNet.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+from model.FeaturesPyramid import FeaturesPyramid
+from model.ResNet50 import ResNet50
+
+
+class RetinaNet(nn.Module):
+    def __init__(self, backbone='resnet50'):
+        super(RetinaNet, self).__init__()
+
+        self.fpn_feature_size = 256
+
+        if backbone == 'resnet50':
+            self.backbone = ResNet50()
+            self.in_channels_size_list = [512, 1024, 2048]
+        else:
+            raise NotImplementedError(f"This backbone ({backbone}) is not implemented.")
+
+        self.fpn = FeaturesPyramid(self.in_channels_size_list, out_channels=self.fpn_feature_size)
+
+    def _make_prediction_layer(self):
+        pass
+
+    def forward(self, x):
+        pass
