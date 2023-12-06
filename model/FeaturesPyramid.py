d2a974e35a01d0384934369389b0079b2cc22b12
jkwiatk1 <01142696@pw.edu.pl>
2023-12-06 02:56:42 +0100

    -Add ResNet50 architecture with pre-trained weights. -Add Feature Pyramid Network (FPN) -Add file to test functionality

---
+++ model/FeaturesPyramid.py
@@ -0,0 +1,36 @@
+import torch.nn as nn
+from typing import List
+
+
+class FeaturesPyramid(nn.Module):
+    def __init__(self, in_channels_list: List[int], out_channels: int):
+        super().__init__()
+
+        self.P3 = nn.Conv2d(in_channels_list[0], out_channels, kernel_size=1)
+        self.P4 = nn.Conv2d(in_channels_list[1], out_channels, kernel_size=1)
+        self.P5 = nn.Conv2d(in_channels_list[2], out_channels, kernel_size=1)
+        self.P6 = nn.Conv2d(in_channels_list[2], out_channels, kernel_size=3, stride=2, padding=1)
+        self.P7 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1)
+
+        self.P5_upsample_2x = nn.Upsample(scale_factor=2, mode='nearest')
+        self.P4_upsample_2x = nn.Upsample(scale_factor=2, mode='nearest')
+        self.P4_reduce_aliasing = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.P3_reduce_aliasing = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+
+    def forward(self, inputs):
+        c3, c4, c5 = inputs
+
+        p6_o = self.P6(c5)
+        p7_o = nn.ReLU()(p6_o)
+        p7_o = self.P7(p7_o)
+
+        p5_o = self.P5(c5)
+        p4_o = self.P4(c4)
+        p3_o = self.P3(c3)
+
+        p5_o_upsampled = self.P5_upsample_2x(p5_o)
+        p4_o = self.P4_reduce_aliasing(p5_o_upsampled + p4_o)
+        p4_o_upsampled = self.P4_upsample_2x(p4_o)
+        p3_o = self.P3_reduce_aliasing(p4_o_upsampled + p3_o)
+
+        return [p3_o, p4_o, p5_o, p6_o, p7_o]
---
+++ model/ResNet50.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class ResNet50(nn.Module):
+    def __init__(self):
+        super(ResNet50, self).__init__()
+        resnet50 = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+        self.conv1 = resnet50.conv1
+        self.bn1 = resnet50.bn1
+        self.relu = resnet50.relu
+        self.maxpool = resnet50.maxpool
+        self.layer1 = resnet50.layer1
+        self.layer2 = resnet50.layer2
+        self.layer3 = resnet50.layer3
+        self.layer4 = resnet50.layer4
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        return x
