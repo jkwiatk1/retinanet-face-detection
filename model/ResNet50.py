ce0e3d5ffcdea92f2e72d47d50195168b89a06dc
jkwiatk1 <01142696@pw.edu.pl>
2023-12-11 20:04:59 +0100

    Add weights freeze

--- model/ResNet50.py
+++ model/ResNet50.py
@@ -7,6 +7,10 @@ class ResNet50(nn.Module):
     def __init__(self):
         super().__init__()
         resnet50 = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+
+        for param in resnet50.parameters():
+            param.requires_grad = False
+
         self.conv1 = resnet50.conv1
         self.bn1 = resnet50.bn1
         self.relu = resnet50.relu
