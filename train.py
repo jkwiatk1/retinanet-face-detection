7bfd8b8d452749950ccfb6d17ba498ff9170d0f1
jkwiatk1 <01142696@pw.edu.pl>
2023-12-12 00:00:14 +0100

    Init train loop

---
+++ train.py
@@ -0,0 +1,84 @@
+import collections
+
+import numpy as np
+import torch
+from torchvision.transforms import Compose, ToTensor, Normalize, ToPILImage, Resize
+import torch.optim as optim
+
+from WiderDataLoader.wider_loader import WiderFaceDataset
+from WiderDataLoader.wider_batch_iterator import BatchIterator
+from model.RetinaNet import RetinaNet
+
+DATA_DIR = 'WIDER'
+NUM_CLASSES = 1
+BATCH_SIZE = 4
+LEARNING_RATE = 1e-5
+EPOCHS_NUM = 10
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+transform = Compose(
+    [ToPILImage(), Resize((800, 1024)), ToTensor(), Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+
+wider_train_dataset = WiderFaceDataset(DATA_DIR, split='train', transform=transform)
+# wider_val_dataset = WiderFaceDataset(DATA_DIR, split='val', transform=transform)
+# wider_test_dataset = WiderFaceDataset(DATA_DIR, split='test', transform=transform)
+
+train_data = BatchIterator(wider_train_dataset, batch_size=BATCH_SIZE, shuffle=False)
+# val_data = BatchIterator(wider_val_dataset, batch_size=BATCH_SIZE, shuffle=False)
+# test_data = BatchIterator(wider_test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+model = RetinaNet().to(device)
+# for _, data in enumerate(train_data):
+#     pred = model(data['img'].cuda().float())
+#     print(pred)
+#     print(type(data['img']))
+#     size = data['img'].shape
+#     print(size)
+#     break
+
+print(model)
+
+optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+model.train()
+
+loss_hist = collections.deque(maxlen=500)
+
+for epoch_num in range(EPOCHS_NUM):
+    model.train()
+
+    epoch_loss = []
+
+    for iter_num, data in enumerate(train_data):
+        optimizer.zero_grad()
+
+        if torch.cuda.is_available():
+            regression_loss, classification_loss = model([data['img'].cuda().float(), data['boxes_list']])
+        else:
+            regression_loss, classification_loss = model([data['img'].float(), data['boxes_list']])
+
+        classification_loss = classification_loss.mean()
+        regression_loss = regression_loss.mean()
+
+        loss = classification_loss + regression_loss
+
+        if bool(loss == 0):
+            continue
+
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.1)
+
+        optimizer.step()
+
+        loss_hist.append(float(loss))
+
+        epoch_loss.append(float(loss))
+
+        print(
+            'Epoch: {} | Iteration: {} | Classification loss: {:1.5f} | Regression loss: {:1.5f} | Running loss: {:1.5f}'.format(
+                epoch_num, iter_num, float(classification_loss), float(regression_loss), np.mean(loss_hist)))
+
+        del classification_loss
+        del regression_loss
